2f8c87791aee8e436982fc1fb5710d38869055ee
Ivan Kalashnikov <ivank.public@gmail.com>
2024-03-19 16:19:51 +0700

    (docs) - description of IContainer and relative topics.

--- src/core/s21_container.h
+++ src/core/s21_container.h
@@ -5,6 +5,49 @@
 
 namespace s21 {
 
+/*
+    The class IContainer serves as an interface for data containers.
+    It defines a set of methods that any subclass representing a container must implement.
+
+    Interface:
+    - Interface in object-oriented programming defines a set
+      of methods that must be implemented by subclasses.
+      It serves as a contract describing the behavior of the class.
+
+    Iterators:
+    - Iterators are used to traverse the elements of a container.
+      They provide a way to sequentially access the elements of a collection
+      and can be used for reading or modifying the values of elements.
+
+    Virtual methods:
+    - Virtual methods are defined using the virtual keyword and can be overridden in subclass.
+      They enable polymorphism, allowing to call methods from the base class for objects of subclass.
+
+    Method descriptions:
+    - size(): returns the number of elements in the container.
+
+    - max_size(): returns the maximum possible number of elements in the container.
+
+    - empty(): returns true if the container is empty, otherwise false.
+
+    - insert(const_iterator pos, const T& value):
+      inserts the element value at the position specified by the iterator pos.
+
+    - clear(): removes all elements from the container.
+
+    - erase(const T& value): removes the element value from the container.
+
+    - swap(IContainer& other) noexcept:
+      swaps the contents of the container with another container.
+
+    - begin():
+      returns an iterator pointing to the first element of the container.
+
+    - end():
+      returns an iterator pointing to the position past the last element of the container.
+
+    - operator=(): assignment operators for copying and moving the container.
+*/
 template <typename T>
 class IContainer {
 public:
