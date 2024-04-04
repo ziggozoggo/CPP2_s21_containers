5d6faf1aaef803688a95c532dd282c01d099c384
Gehn Aelinor <gehnaeli@student.21-school.ru>
2024-04-04 13:05:52 +0700

    (dev) - MockClass for tests complete.

--- src/unit_tests/suites-core/s21_containers_core_test.h
+++ src/unit_tests/suites-core/s21_containers_core_test.h
@@ -9,18 +9,62 @@
 
 class MockClass {
 public:
-  const int size_ = 10;
-  int* data_;
 
-  MockClass() : data_(new int[size_]) {
+  MockClass() : data_(new int[size_]), subData_(42) {
     for (int i = 0; i < size_; i++) {
       data_[i] = i * 2;
     }
   }
 
-  ~MockClass() {
-    delete[] data_;
+  MockClass(int subData) : data_(new int[size_]), subData_(subData) {
+    for (int i = 0; i < size_; i++) {
+      data_[i] = i * 2;
+    }
   }
+
+  MockClass(const MockClass& other) : data_(new int[size_]), subData_(other.subData_) {
+    for (int i = 0; i < size_; i++) {
+      data_[i] = other.data_[i];
+    }
+  }
+
+  MockClass(MockClass&& other) noexcept : data_(other.data_), subData_(other.subData_) {
+    other.data_ = nullptr;
+    other.subData_ = 0;
+  }
+
+  ~MockClass() {
+    if (data_ != nullptr) delete[] data_;
+    data_ = nullptr;
+  }
+
+  MockClass& operator=(const MockClass& other) {
+    if (this != &other) {
+      delete[] data_;
+      data_ = new int[size_];
+      for (int i = 0; i < size_; i++) {
+        data_[i] = other.data_[i];
+      }
+      subData_ = other.subData_;
+    }
+    return *this;
+  }
+
+  MockClass& operator=(MockClass&& other) noexcept {
+    if (this != &other) {
+      delete[] data_;
+      data_ = other.data_;
+      subData_ = other.subData_;
+      other.data_ = nullptr;
+      other.subData_ = 0;
+    }
+    return *this;
+  }
+
+private:
+  const int size_ = 10;
+  int* data_;
+  int subData_;
 };
 
 #endif  // SRC_UNIT_S21_MATRIX_CORE_TEST_H_
