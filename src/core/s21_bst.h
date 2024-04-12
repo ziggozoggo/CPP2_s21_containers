95cf6e0c2333a36a1ed3c1dafaa48128f7f5aaab
Harmund Albertha <harmunda@student.21-school.ru>
2024-04-12 14:50:06 +0700

    RBT template

--- src/core/s21_bst.h
+++ src/core/s21_bst.h
@@ -1,117 +1,286 @@
-#ifndef SRC_S21_BST_H_
-#define SRC_S21_BST_H_
+#ifndef S21_BST_H_
+#define S21_BST_H_
 
 #include <iostream>
+#include <algorithm>
 #include <optional>
 
 namespace s21 {
+  template <typename T>
+  class BST {
+  private:
+    enum class NodeColor {
+      BLACK,
+      RED
+    };
 
-template <typename T>
-class BST {
-public:
-  BST() : root_(nullptr) {}
-  ~BST() { deleteTree(root_); }
+    struct Node {
+      T value;
+      Node* left;
+      Node* right;
+      Node* parent;
+      NodeColor color = NodeColor::BLACK;
 
-  bool isEmpty() { return root_ == nullptr; }
+      Node(): left(nullptr), right(nullptr), parent(nullptr) {}
+      Node(T val, Node* nil_): value(val), left(nil_), right(nil_), parent(nil_), color(NodeColor::RED) {}
+    };
 
-  void insert(const T& val) {
-    root_ = insertRecursive(root_, val);
-  }
+    Node* nil = new Node();
+    Node* root;
+   
+  public:
+    BST(): root(nil) {}
+    BST(T val): root(new Node(val, nil)) {}
 
-  std::optional<T> search(const T& val) {
-    return searchRecursive(root_, val);
-  }
-
-  void remove(const T& val) {
-    root_ = removeRecursive(root_, val);
-  }
-
-  void printTree() {
-    printInOrder(root_);
-  }
-
-private:
-  struct Node {
-    T val_;
-    Node* left_;
-    Node* right_;
-
-    Node(T val) : val_(val), left_(nullptr), right_(nullptr) {}
-  };
-
-  Node* root_;
-
-  Node* insertRecursive(Node* node, const T& val) {
-    if (node == nullptr) return new Node(val);
-
-    if (val < node->val_) {
-      node->left_ = insertRecursive(node->left_, val);
-    } else if (val > node->val_) {
-      node->right_ = insertRecursive(node->right_, val);
+    ~BST() {  
+      deleteTree(root);
+      delete nil;
     }
 
-    return node;
-  }
+    bool isEmpty() { return root == nullptr; }
 
-  std::optional<T> searchRecursive(Node* node, const T& val) {
-    if (node == nullptr) return std::nullopt;
+    void insert(const T& val) {
+      insertNode(val);
+    }
 
-    if (node->val_ == val) return node->val_;
-    else if (val < node->val_) return searchRecursive(node->left_, val);
-    else return searchRecursive(node->right_, val);
-  }
+    std::optional<Node*> search(const T& val) {
+      return search(root, val);
+    }
 
-  Node* removeRecursive(Node* node, const T& val) {
-    if (node == nullptr) return nullptr;
+    void printTree() {
+      printTree(root);
+    }
 
-    if (val < node->val_) {
-      node->left_ = removeRecursive(node->left_, val);
-    } else if (val > node->val_) {
-      node->right_ = removeRecursive(node->right_, val);
-    } else {
-      if (node->left_ == nullptr) {
-        Node* temp = node->right_;
-        delete node;
-        return temp;
-      } else if (node->right_ == nullptr) {
-        Node* temp = node->left_;
-        delete node;
-        return temp;
+    void remove(const T& val) {
+      removeNode(val);
+    }
+
+  private:
+    bool isEmpty(Node* node) { return node == nil; }
+
+    void insertNode(const T& val) {
+      Node* currentNode = root;
+      Node* parent = nil;
+      while (!isEmpty(currentNode)) {
+        parent = currentNode;
+        (val < currentNode->value) ? currentNode = currentNode->left
+        : currentNode = currentNode->right;
       }
-
-      Node* temp = minValueNode(node->right_);
-      node->val_ = temp->val_;
-      node->right_ = removeRecursive(node->right_, temp->val_);
+      if (parent->value == val) return;
+      
+      Node* newNode = new Node(val, nil);
+      newNode->parent = parent;
+      if (parent == nil) root = newNode;
+      else if (val < parent->value) parent->left = newNode;
+      else parent->right = newNode;
+      balanceInsert(newNode);
     }
 
-    return node;
-  }
-
-    Node* minValueNode(Node* node) {
-    Node* current = node;
-    while (current->left_ != nullptr) {
-      current = current->left_;
+    void balanceInsert(Node* newNode) {
+      Node* uncle;
+      while (newNode->parent->color == NodeColor::RED) {
+        if (newNode->parent == newNode->parent->parent->left) {
+          uncle = newNode->parent->parent->right;
+          if (uncle->color == NodeColor::RED) {
+            newNode->parent->color = NodeColor::BLACK;
+            uncle->color = NodeColor::BLACK;
+            newNode->parent->parent->color = NodeColor::RED;
+            newNode = newNode->parent->parent;
+          } else {
+            if (newNode == newNode->parent->right) {
+              leftRotate(newNode->parent);
+            }
+            newNode->parent->color = NodeColor::BLACK;
+            newNode->parent->parent->color = NodeColor::RED;
+            rightRotate(newNode->parent->parent);
+          }
+        } else {
+          uncle = newNode->parent->parent->left;
+          if (uncle->color == NodeColor::RED) {
+            newNode->parent->color = NodeColor::BLACK;
+            uncle->color = NodeColor::BLACK;
+            newNode->parent->parent->color = NodeColor::RED;
+            newNode = newNode->parent->parent;
+          } else {
+            if (newNode == newNode->parent->left) {
+              rightRotate(newNode->parent);
+            }
+            newNode->parent->color = NodeColor::BLACK;
+            newNode->parent->parent->color = NodeColor::RED;
+            leftRotate(newNode->parent->parent);
+          }
+        }
+      }
+      root->color = NodeColor::BLACK;
     }
-    return current;
-  }
 
-    void printInOrder(Node* node) {
-    if (node == nullptr) return;
+    std::optional<Node*> search(Node* node, const T& val) {
+      if (isEmpty(node)) return std::nullopt;
 
-    printInOrder(node->left_);
-    std::cout << node->val_ << " ";
-    printInOrder(node->right_);
-  }
+      if (node->value == val) return node;
+      return (val < node->value) ? search(node->left, val) : search(node->right, val);
+    }
 
-  void deleteTree(Node* node) {
-    if (node == nullptr) return;
+    void printTree(Node* node) {
+      if (isEmpty(node)) return;
 
-    deleteTree(node->left_);
-    deleteTree(node->right_);
-    delete node;
-  }
-};
+      printTree(node->left);
+      std::cout << node->value << ' ';
+      printTree(node->right);
+    }
 
+    int getChildrenCount(Node* node) {
+      int count = 0;
+      if (!isEmpty(node->left)) count++;
+      if (!isEmpty(node->right)) count++;
+      return count;
+    }
+
+    Node* getChildOrMock(Node* node) {
+      return isEmpty(node->left) ? node->right : node->left;
+    }
+
+    void transplateNode(Node* dest, Node* src) {
+      if (dest == root) root = src;
+      else if (dest == dest->parent->left) dest->parent->left = src;
+      else dest->parent->right = src;
+      src->parent = dest->parent;
+      delete dest;
+    }
+
+    void removeNode(const T& val) {
+      std::optional<Node*> nodeToDeleteOpt = search(val);
+      if (!nodeToDeleteOpt.has_value()) return;
+      Node* nodeToDelete = *nodeToDeleteOpt;
+
+      NodeColor removedNodeColor = nodeToDelete->color;
+      Node* child;
+
+      if (getChildrenCount(nodeToDelete) < 2) {
+        child = getChildOrMock(nodeToDelete);
+        transplateNode(nodeToDelete, child);
+      } else {
+        Node* minNode = getMin(nodeToDelete->right);
+        nodeToDelete->value = minNode->value;
+        removedNodeColor = minNode->color;
+        child = getChildOrMock(minNode);
+        transplateNode(minNode, child);
+      }
+      if (removedNodeColor == NodeColor::BLACK) balanceRemove(child);
+    }
+
+    void balanceRemove(Node* node) {
+      while (node != root && node->color == NodeColor::BLACK) {
+        Node* brother;
+        if (node == node->parent->left) {
+          brother = node->parent->right;
+          if (brother->color == NodeColor::RED) {
+            brother->color = NodeColor::BLACK;
+            node->parent->color = NodeColor::RED;
+            leftRotate(node->parent);
+            brother = node->parent->right;
+          }
+          if (brother->left->color == NodeColor::BLACK && brother->right->color == NodeColor::BLACK) {
+            brother->color = NodeColor::RED;
+            node = node->parent;
+          } else {
+            if (brother->right->color == NodeColor::BLACK) {
+              brother->left->color = NodeColor::BLACK;
+              brother->color = NodeColor::RED;
+              rightRotate(brother);
+              brother = node->parent->right;
+            }
+            brother->color = node->parent->color;
+            node->parent->color = NodeColor::BLACK;
+            brother->right->color = NodeColor::BLACK;
+            leftRotate(node->parent);
+            node = root;
+          }
+        } else {
+          brother = node->parent->left;
+          if (brother->color == NodeColor::RED) {
+            brother->color = NodeColor::BLACK;
+            node->parent->color = NodeColor::RED;
+            rightRotate(node->parent);
+            brother = node->parent->left;
+          }
+          if (brother->left->color == NodeColor::BLACK && brother->right->color == NodeColor::BLACK) {
+            brother->color = NodeColor::RED;
+            node = node->parent;
+          } else {
+            if (brother->left->color == NodeColor::BLACK) {
+              brother->right->color = NodeColor::BLACK;
+              brother->color = NodeColor::RED;
+              leftRotate(brother);
+              brother = node->parent->left;
+            }
+            brother->color = node->parent->color;
+            node->parent->color = NodeColor::BLACK;
+            brother->left->color = NodeColor::BLACK;
+            rightRotate(node->parent);
+            node = root;
+          }
+        }
+      }
+      node->color = NodeColor::BLACK;
+    }
+
+    Node* getMin(Node* node) {
+      if (isEmpty(node)) return node;
+
+      if (isEmpty(node->left)) return node;
+      return getMin(node->left);
+    }
+
+    Node* getMax(Node* node) {
+      if (isEmpty(node)) return nullptr;
+
+      if (node->right == nullptr) return node;
+      return getMax(node->right);
+    }
+
+    void deleteTree(Node* node) {
+      if (isEmpty(node)) return;
+
+      deleteTree(node->left);
+      deleteTree(node->right);
+      delete node;
+    }
+
+    void swap(Node* a, Node* b) {
+      T temp = a->value;
+      NodeColor tempColor = a->color;
+
+      a->color = b->color;
+      b->color = tempColor;
+      a->value = b->value;
+      b->value = temp;
+    }
+
+    void rightRotate(Node* node) {
+      swap(node, node->left);
+      Node* temp = node->right;
+      node->right = node->left;
+      node->left = node->right->left;
+      node->right->left = node->right->right;
+      node->right->right = temp;
+      
+      node->left->parent = node;
+      node->right->right->parent = node->right;
+    }
+
+    void leftRotate(Node* node) {
+      swap(node, node->right);
+      Node* temp = node->left;
+      node->left = node->right;
+      node->right = node->left->right;
+      node->left->right = node->left->left;
+      node->left->left = temp;
+      
+      node->right->parent = node;
+      node->left->left->parent = node->left;
+    }
+  };
 }
 
-#endif  // SRC_S21_BST_H_
+#endif //S21_BST_H_
--- src/core/train.cc
+++ src/core/train.cc
@@ -1,49 +1,79 @@
-#include "train.h"
+#include "s21_bst.h"
+#include <set>
 
 int main() {
-    s21::BST btree;
+    s21::BST<char> btree;
+
+    std::set <char> mySet;
+    mySet.insert('a');
+    mySet.erase('c');
+
 
     // std::cout << btree.root->value;
 
-    // btree.insert(15);
-    // btree.insert(8);
-    // btree.insert(18);
-    // btree.insert(4);
-    // btree.insert(9);
-    // btree.insert(11);
-    // btree.insert(12);
+    btree.insert('a');
+    btree.insert('b');
+    btree.insert('h');
+    btree.insert('z');
+    btree.insert('e');
+    btree.insert('u');
+    btree.insert('x');
 
-    btree.insert(10);
-    btree.insert(20);
-    btree.insert(30);
-    btree.insert(40);
-    btree.insert(25);
-    btree.insert(35);
-    btree.insert(45);
-    btree.insert(5);
-    btree.insert(50);
-    btree.insert(42);
-    btree.insert(48);
+    // btree.insert(10);
+    // btree.insert(20);
+    // btree.insert(30);
+    // btree.insert(40);
+    // btree.insert(25);
+    // btree.insert(35);
+    // btree.insert(45);
+    // btree.insert(5);
+    // btree.insert(50);
+    // btree.insert(42);
+    // btree.insert(48);
 
     btree.printTree();
     std::cout << std::endl;
 
-    btree.remove(25);
-    btree.remove(45);
-    btree.remove(40);
-    btree.remove(35);
-    btree.remove(10);
-    btree.remove(30);
-    btree.remove(5);
-    btree.remove(48);
-    btree.remove(42);
-    btree.remove(50);
-    btree.remove(20);
+    btree.remove('p');
+
+    btree.remove('h');
+    btree.remove('b');
+    btree.remove('u');
+    btree.remove('a');
+    btree.remove('z');
+    btree.remove('x');
+    btree.remove('e');
+
+    // btree.remove(25);
+    // btree.remove(45);
+    // btree.remove(40);
+    // btree.remove(35);
+    // btree.remove(10);
+    // btree.remove(30);
+    // btree.remove(5);
+    // btree.remove(48);
+    // btree.remove(42);
+    // btree.remove(50);
+    // btree.remove(20);
 
     btree.printTree();
     std::cout << std::endl;
     
+    s21::BST<std::string> bst;
+    bst.insert("abc");
+    bst.insert("cba");
+    bst.insert("asdgaer");
+    bst.insert("gafergf");
+    bst.insert("grtsgfsdgvbaerg");
+    bst.insert("temp");
+    bst.insert("temp2");
+    bst.insert("temporary");
+    bst.insert("abccca");
+    bst.insert("dfed");
+    
 
+
+    // std::cout << bst.search(5.54).value()->value;
 
     
 
